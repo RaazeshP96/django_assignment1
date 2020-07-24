11643fd5221f844e1f23f9032409cfea8b6f60cf
Raazeshp96 <raazeshp96@gmail.com>
2020-07-24 22:53:22 +0545

    email authintication

--- account/models.py
+++ account/models.py
@@ -1,13 +1,54 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.core.validators import validate_email
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomManager(BaseUserManager):
+    def create_user(self, email, password=None, is_active=True, is_staff=False, is_admin=False):
+        if not email:
+            raise ValueError("Users must have an email address")
+        if not password:
+            raise ValueError("Users must have a password")
+
+        user_obj = self.model(
+            email=self.normalize_email(email)
+        )
+        user_obj.set_password(password)  # change user password
+        user_obj.staff = is_staff
+        user_obj.admin = is_admin
+        user_obj.active = is_active
+        user_obj.save(using=self._db)
+        return user_obj
+
+    def create_staffuser(self, email, password=None):
+        user = self.create_user(
+            email,
+            password=password,
+            is_staff=True
+        )
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(
+            email,
+            password=password,
+            is_staff=True,
+            is_admin=True
+        )
+        return user
 
 
 class CustomUser(AbstractUser):
+    email = models.EmailField(
+        _('email address'),
+        validators=[validate_email],
+        help_text=_('for eg: abcd@abc.abc'),
+        unique=True
+    )
     profile_image = models.ImageField(default='', upload_to='profile_pic')
     EMAIL_FIELD = 'email'
-    USERNAME_FIELD='username'
-    REQUIRED_FIELDS = ['email']
-
-
-# class CustomManager(UserManager):
-#     pass
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    objects = CustomManager()
--- account/views.py
+++ account/views.py
@@ -1,12 +1,15 @@
 from django.views.generic import CreateView
 from .forms import CustomUserChangeForm, CustomUserCreationForm
+from django.urls import reverse_lazy
 
 
 class SignupPageView(CreateView):
     form_class = CustomUserCreationForm
     template_name = 'account/customuser_form.html'
+    success_url = reverse_lazy('blog-home')
 
 
 class Profile(CreateView):
     form_class = CustomUserChangeForm
     template_name = 'account/profile.html'
+    success_url = reverse_lazy('blog-home')
